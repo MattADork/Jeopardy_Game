b504fc004e093b9e923b454d6b193326cb78f2cf
Matt Dove <mattdove22@gmail.com>
2021-04-04 17:10:18 -0700

    initialized board, found coordinates for first row

---
+++ main.rb
@@ -0,0 +1,15 @@
+require_relative 'board'
+board = BOARD
+puts board
+
+
+
+board[528..531] = "    "
+board[545..548] = "    "
+board[562..565] = "    "
+board[579..582] = "    "
+board[596..599] = "    "
+board[613..616] = "    "
+
+
+puts board
